7b9caea7a694cc10e636a606c6baf14b3f1638bf
Derek Kraan <derek.kraan@gmail.com>
2018-05-08 17:06:50 +0200

    Add missing package metadata

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Horde.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -23,6 +24,15 @@ defmodule Horde.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  def package do
+    [
+      description: "Distributed process registry taking advantage of δ-CRDTs",
+      licenses: ["MIT"],
+      maintainers: ["Derek Kraan"],
+      links: %{github: "https://github.com/derekkraan/horde"}
     ]
   end
 end
