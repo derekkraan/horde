f5379b5c0db6f352bb62caf533b3696c9b8f99f2
Derek Kraan <derek.kraan@gmail.com>
2018-05-14 16:12:43 +0200

    Track hordes using a delta CRDT

--- lib/horde.ex
+++ lib/horde.ex
@@ -1,18 +1,83 @@
 defmodule Horde do
   @moduledoc """
-  Documentation for Horde.
+  Horde is a distributed process registry that takes advantage of Delta-CRDTs.
   """
 
-  @doc """
-  Hello world.
+  alias DeltaCrdt.{CausalCrdt, AddWinsFirstWriteWinsMap, ObservedRemoveMap}
 
-  ## Examples
+  defmodule State do
+    defstruct node_id: nil,
+              members_pid: nil,
+              members: %{}
+  end
 
-      iex> Horde.hello
-      :world
+  def child_spec(node_id) do
+    %{id: node_id, start: {__MODULE__, :start_link, [node_id]}}
+  end
 
-  """
-  def hello do
-    :world
+  def start_link(node_id) do
+    GenServer.start_link(__MODULE__, node_id)
+  end
+
+  def join_hordes(horde, other_horde) do
+    GenServer.cast(horde, {:join_horde, other_horde})
+  end
+
+  def members(horde) do
+    GenServer.call(horde, :members)
+  end
+
+  ### GenServer callbacks
+
+  def init(node_id) do
+    {:ok, members_pid} = CausalCrdt.start_link(%ObservedRemoveMap{}, {self(), :members_updated})
+
+    GenServer.cast(
+      members_pid,
+      {:operation, {AddWinsFirstWriteWinsMap, :add, [node_id, {members_pid}]}}
+    )
+
+    {:ok,
+     %State{
+       node_id: node_id,
+       members_pid: members_pid,
+       members: %{node_id => {members_pid}}
+     }}
+  end
+
+  def handle_cast(
+        {:request_to_join_horde, {other_node_id, other_members_pid}},
+        state
+      ) do
+    send(state.members_pid, {:add_neighbour, other_members_pid})
+    send(state.members_pid, :ship_interval_or_state_to_all)
+    {:noreply, state}
+  end
+
+  def handle_info(:members_updated, state) do
+    members = GenServer.call(state.members_pid, {:read, AddWinsFirstWriteWinsMap})
+
+    member_pids =
+      Enum.map(members, fn {_key, {pid}} -> pid end)
+      |> Enum.into(MapSet.new())
+
+    state_member_pids =
+      Enum.map(state.members, fn {_node_id, {pid}} -> pid end) |> Enum.into(MapSet.new())
+
+    if !MapSet.equal?(member_pids, state_member_pids) do
+      send(state.members_pid, {:add_neighbours, member_pids})
+      send(state.members_pid, :ship_interval_or_state_to_all)
+    end
+
+    {:noreply, %{state | members: members}}
+  end
+
+  def handle_cast({:join_horde, other_horde}, state) do
+    GenServer.cast(other_horde, {:request_to_join_horde, {state.node_id, state.members_pid}})
+    {:noreply, state}
+  end
+
+  def handle_call(:members, _from, state) do
+    {:reply, {:ok, state.members}, state}
   end
 end
--- test/horde_test.exs
+++ test/horde_test.exs
@@ -2,7 +2,37 @@ defmodule HordeTest do
   use ExUnit.Case
   doctest Horde
 
-  test "greets the world" do
-    assert Horde.hello() == :world
+  test "two hordes can join each other" do
+    {:ok, horde_1} = Horde.start_link(:horde_1)
+    {:ok, horde_2} = Horde.start_link(:horde_2)
+    Horde.join_hordes(horde_1, horde_2)
+    Process.sleep(10)
+    {:ok, members} = Horde.members(horde_2)
+    assert [:horde_1, :horde_2] = Map.keys(members)
+  end
+
+  test "three hordes can join in one giant horde" do
+    {:ok, horde_1} = Horde.start_link(:horde_1)
+    {:ok, horde_2} = Horde.start_link(:horde_2)
+    {:ok, horde_3} = Horde.start_link(:horde_3)
+    Horde.join_hordes(horde_1, horde_2)
+    Horde.join_hordes(horde_2, horde_3)
+    Process.sleep(10)
+    {:ok, members} = Horde.members(horde_2)
+    assert [:horde_1, :horde_2, :horde_3] = Map.keys(members)
+  end
+
+  test "25 hordes can join in one gargantuan horde" do
+    last_horde =
+      1..25
+      |> Enum.reduce(nil, fn x, last_horde ->
+        {:ok, horde} = Horde.start_link(:"horde_#{x}")
+        if last_horde, do: Horde.join_hordes(horde, last_horde)
+        horde
+      end)
+
+    Process.sleep(1000)
+    {:ok, members} = Horde.members(last_horde)
+    assert 25 = Enum.count(members)
   end
 end
