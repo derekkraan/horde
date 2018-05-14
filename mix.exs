1ea523063b22a0cfadce441451a00f5de7499c28
Derek Kraan <derek.kraan@gmail.com>
2018-05-14 08:32:39 +0200

    Add delta_crdt as dependency

--- mix.exs
+++ mix.exs
@@ -22,8 +22,7 @@ defmodule Horde.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:delta_crdt, git: "https://github.com/derekkraan/delta_crdt_ex"}
     ]
   end
 
