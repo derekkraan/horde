1ed4af9b4b16ae1481ba1ec9257d3c6078359b99
Ethan Mahintorabi <ethan.mahintorabi@ibm.com>
2019-01-19 22:07:48 -0800

    update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Horde.MixProject do
     [
       app: :horde,
       version: "0.4.0-rc.2",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
