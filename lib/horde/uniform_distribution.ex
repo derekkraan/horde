021504e5011eab3d0399581217db0b31c7cd3c1e
Derek Kraan <derek.kraan@gmail.com>
2019-06-07 11:02:17 +0200

    Use libring to distribute processes over nodes.

--- lib/horde/uniform_distribution.ex
+++ lib/horde/uniform_distribution.ex
@@ -7,27 +7,32 @@ defmodule Horde.UniformDistribution do
 
   def choose_node(identifier, members) do
     members =
-      members
-      |> Enum.filter(fn
-        %{status: :alive} -> true
-        _ -> false
-      end)
-      |> Enum.sort_by(fn %{name: name} -> name end)
+      filter_members(members)
+      |> Map.new(fn member -> {member.name, member} end)
 
     case Enum.count(members) do
       0 ->
         {:error, :no_alive_nodes}
 
       count ->
-        index = hash(term_to_string_identifier(identifier)) |> rem(count)
-        {:ok, Enum.at(members, index)}
+        chosen_member =
+          HashRing.new()
+          |> HashRing.add_nodes(Map.keys(members))
+          |> HashRing.key_to_node(identifier)
+
+        {:ok, Map.get(members, chosen_member)}
     end
+  end
+
+  defp filter_members(members) do
+    Enum.filter(members, fn
+      %{status: :alive} -> true
+      _ -> false
+    end)
   end
 
   defp hash(identifier) when is_integer(identifier), do: identifier
   defp hash(identifier), do: :erlang.phash2(identifier)
 
   def has_quorum?(_members), do: true
-
-  defp term_to_string_identifier(term), do: term |> :erlang.term_to_binary() |> Base.encode16()
 end
