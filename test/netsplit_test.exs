379969bdc50f49d94005c10f4eb6d3efb93ef1c8
Pedro Gutiérrez <pedro.gutierrez@erlang-solutions.com>
2019-10-17 23:13:55 +0200

    added netsplit/name conflict test

--- test/netsplit_test.exs
+++ test/netsplit_test.exs
@@ -40,4 +40,71 @@ defmodule NetsplitTest do
     assert_receive {^other_node, :hello_echo_server}, 1100
     refute_receive {^n, :hello_echo_server}, 1100
   end
+
+  test "name conflict after healing netsplit" do
+    cluster_name = "cluster"
+    server_name = "server"
+    sleep_millis = 2000
+
+    [node1, node2, node3] = nodes = LocalCluster.start_nodes(cluster_name, 3)
+
+    Enum.each(nodes, fn node ->
+      assert :pong = Node.ping(node)
+    end)
+
+    # Start a test supervision tree in all three nodes
+    Enum.each(nodes, fn node ->
+      Node.spawn(node, LocalClusterHelper, :start, [
+        MySupervisionTree,
+        :start_link,
+        [[cluster: cluster_name, distribution: Horde.UniformQuorumDistribution, sync_interval: 5]]
+      ])
+    end)
+
+    Enum.each(nodes, fn node ->
+      assert MySupervisor.alive?(node)
+      assert MyRegistry.alive?(node)
+    end)
+
+    # Wait for supervisor and registry in all nodes
+    Process.sleep(sleep_millis)
+
+    Schism.partition([node1, node2])
+    Schism.partition([node3])
+
+    Process.sleep(sleep_millis)
+
+    Enum.each(nodes, fn node ->
+      assert MySupervisor.alive?(node)
+      assert MyRegistry.alive?(node)
+    end)
+
+    # Create a server with the same name in both partitions
+    {:ok, pid1} = MyCluster.start_server(node1, server_name)
+    {:ok, pid2} = MyCluster.start_server(node3, server_name)
+
+    assert Enum.member?([node1, node2], node(pid1))
+    assert node3 == node(pid2)
+
+    Process.sleep(sleep_millis)
+
+    # Heal the cluster
+    Schism.heal(nodes)
+    Process.sleep(sleep_millis)
+
+    Enum.each(nodes, fn node ->
+      assert MySupervisor.alive?(node)
+      assert MyRegistry.alive?(node)
+    end)
+
+    # Verify all nodes are able to see the same pid for that server
+    pid1 = MyCluster.whereis(node1, server_name)
+    pid2 = MyCluster.whereis(node2, server_name)
+    pid3 = MyCluster.whereis(node3, server_name)
+
+    assert is_pid(pid1)
+    assert pid1 == pid2
+    assert pid1 == pid3
+    assert pid2 == pid3
+  end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,6 @@
 # Disable until we can get this working on CircleCI
-# :ok = LocalCluster.start()
+:ok = LocalCluster.start()
 
-# Application.ensure_all_started(:horde)
+Application.ensure_all_started(:horde)
 
 ExUnit.start()
