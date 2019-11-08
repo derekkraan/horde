2ba65e5992059bc071d401b9a26876a46da3f03b
Jaap Frolich <jfrolich@gmail.com>
2019-11-08 16:46:22 +0800

    Fix documentation #2

--- lib/horde/registry.ex
+++ lib/horde/registry.ex
@@ -21,8 +21,8 @@ defmodule Horde.Registry do
   defmodule MyRegistry do
     use Horde.Registry
 
-    def start_link(init_arg, options \\ []) do
-      Horde.Registry.start_link(__MODULE__, init_arg, options)
+    def start_link(_) do
+      Horde.Registry.start_link(__MODULE__, [keys: :unique], name: __MODULE__)
     end
 
     def init(init_arg) do
@@ -32,8 +32,9 @@ defmodule Horde.Registry do
     end
 
     defp members() do
-      # ...
-    end
+      [Node.self() | Node.list()]
+      |> Enum.map(fn node -> {__MODULE__, node} end)
+    end  
   end
   ```
 
