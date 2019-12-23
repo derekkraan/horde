30e80a88bc5463343f7b3c8336651b434fd89bc0
Derek Kraan <derek.kraan@gmail.com>
2019-12-23 10:22:21 +0100

    Callback should be `{:ok, options()} | :ignore`, this satisfies dialyzer

--- lib/horde/dynamic_supervisor.ex
+++ lib/horde/dynamic_supervisor.ex
@@ -65,11 +65,7 @@ defmodule Horde.DynamicSupervisor do
           | {:delta_crdt_options, [DeltaCrdt.crdt_option()]}
           | {:process_redistribution, :active | :passive}
 
-  @callback init(options()) ::
-              {:ok, map()}
-              | {:ok, tuple()}
-              | :ignore
-              | {:stop, {:bad_return, {atom(), :init, any()}}}
+  @callback init(options()) :: {:ok, options()} | :ignore
   @callback child_spec(options :: options()) :: Supervisor.child_spec()
 
   defmacro __using__(options) do
