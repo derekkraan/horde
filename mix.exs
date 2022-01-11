b86adf17f4d687fc1691d93b53e4cdb155cd1454
Derek Kraan <derek.kraan@gmail.com>
2022-01-11 14:59:29 +0100

    Bump version to 0.8.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Horde.MixProject do
   def project do
     [
       app: :horde,
-      version: "0.8.5",
+      version: "0.8.6",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
