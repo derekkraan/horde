c6f13f4d0d4929e1dd68a025ec7c8462a0edbf88
gavriel@trixta.com <gavriel@trixta.com>
2024-06-14 09:25:18 +0200

    added child_restart_matches_dynamic_supervisor.exs

---
+++ test/child_restart_matches_dynamic_supervisor.exs
@@ -0,0 +1,85 @@
+defmodule ClusterTest do
+  use ExUnit.Case, async: false
+
+  defmodule BasicGenServer do
+    use GenServer
+
+    def init(init_arg) do
+      {:ok, init_arg}
+    end
+
+    def start_link(arg) do
+      GenServer.start_link(__MODULE__, arg)
+    end
+  end
+
+
+  test "DynamicSupervisor vs Horde.DynamicSupervisor with pids" do
+    name = :"supervisor_#{:rand.uniform(100_000_000)}"
+    {:ok, sup_pid} = DynamicSupervisor.start_link(name: name, strategy: :one_for_one)
+
+    {:ok, child1} = DynamicSupervisor.start_child(sup_pid, %{id: :child1, start: {BasicGenServer, :start_link, [0]}})
+    {:ok, child2} = DynamicSupervisor.start_child(sup_pid, %{id: :child2, start: {BasicGenServer, :start_link, [0]}})
+    {:ok, child3} = DynamicSupervisor.start_child(sup_pid, %{id: :child3, start: {BasicGenServer, :start_link, [0]}})
+
+    true = Process.exit(child3, :kill)
+    Process.sleep(1000)
+    assert Process.alive?(child1)
+    assert Process.alive?(child2)
+    assert !Process.alive?(child3)
+    children = DynamicSupervisor.which_children(sup_pid)
+
+    assert Enum.count(children, fn {_id, _pid, _type, mod} -> [BasicGenServer] == mod end) == 3
+    name = :"supervisor_#{:rand.uniform(100_000_000)}"
+    {:ok, sup_pid} = Horde.DynamicSupervisor.start_link(name: name, strategy: :one_for_one)
+
+    {:ok, child1} = Horde.DynamicSupervisor.start_child(sup_pid, %{id: :child1, start: {BasicGenServer, :start_link, [0]}})
+    {:ok, child2} = Horde.DynamicSupervisor.start_child(sup_pid, %{id: :child2, start: {BasicGenServer, :start_link, [0]}})
+    {:ok, child3} = Horde.DynamicSupervisor.start_child(sup_pid, %{id: :child3, start: {BasicGenServer, :start_link, [0]}})
+
+    true = Process.exit(child3, :kill)
+    Process.sleep(1000)
+    assert Process.alive?(child1)
+    assert Process.alive?(child2)
+    assert !Process.alive?(child3)
+    children = Horde.DynamicSupervisor.which_children(sup_pid)
+
+    assert Enum.count(children, fn {_id, _pid, _type, mod} -> [BasicGenServer] == mod end) == 3
+  end
+
+  test "DynamicSupervisor vs Horde.DynamicSupervisor with names" do
+    name = :"supervisor_#{:rand.uniform(100_000_000)}"
+
+    {:ok, _sup_pid} = DynamicSupervisor.start_link(name: name, strategy: :one_for_one)
+
+    {:ok, child1} = DynamicSupervisor.start_child(name, %{id: :child1, start: {BasicGenServer, :start_link, [0]}})
+    {:ok, child2} = DynamicSupervisor.start_child(name, %{id: :child2, start: {BasicGenServer, :start_link, [0]}})
+    {:ok, child3} = DynamicSupervisor.start_child(name, %{id: :child3, start: {BasicGenServer, :start_link, [0]}})
+
+    true = Process.exit(child3, :kill)
+    Process.sleep(1000)
+    assert Process.alive?(child1)
+    assert Process.alive?(child2)
+    assert !Process.alive?(child3)
+    children = DynamicSupervisor.which_children(name)
+
+    assert Enum.count(children, fn {_id, _pid, _type, mod} -> [BasicGenServer] == mod end) == 3
+    name = :"supervisor_#{:rand.uniform(100_000_000)}"
+
+    {:ok, _sup_pid} = Horde.DynamicSupervisor.start_link(name: name, strategy: :one_for_one)
+
+    {:ok, child1} = Horde.DynamicSupervisor.start_child(name, %{id: :child1, start: {BasicGenServer, :start_link, [0]}})
+    {:ok, child2} = Horde.DynamicSupervisor.start_child(name, %{id: :child2, start: {BasicGenServer, :start_link, [0]}})
+    {:ok, child3} = Horde.DynamicSupervisor.start_child(name, %{id: :child3, start: {BasicGenServer, :start_link, [0]}})
+
+    true = Process.exit(child3, :kill)
+    Process.sleep(1000)
+    assert Process.alive?(child1)
+    assert Process.alive?(child2)
+    assert !Process.alive?(child3)
+    children = Horde.DynamicSupervisor.which_children(name)
+
+    assert Enum.count(children, fn {_id, _pid, _type, mod} -> [BasicGenServer] == mod end) == 3
+
+  end
+end
